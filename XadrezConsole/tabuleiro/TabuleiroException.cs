3bb7c471a6a0c504f4e3e77ca97b47bd113503b4
Raphael Rocha <rocharaphael0911@gmail.com>
2022-07-21 18:46:08 -0300

    Tratando erro com exceção personalizada

---
+++ XadrezConsole/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace XadrezConsole.tabuleiro
+{
+    internal class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+            {
+            }
+    }
+}
